de4f5ec088daf050430a764fa6a2aa8e07bc6926
이상원 <sangwon020214@gmail.com>
2025-07-01 22:20:47 +0900

    design: 홈페이지 배너 디자인 수정

--- src/components/BannerCarousel.tsx
+++ src/components/BannerCarousel.tsx
@@ -1,42 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import banner1 from "../assets/banner1.jpg";
 import banner2 from "../assets/banner2.jpg";
 import banner3 from "../assets/banner3.jpg";
-
-const banners = [banner1, banner2, banner3];
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const BannerCarousel: React.FC = () => {
-  const [idx, setIdx] = useState(0);
-
   return (
-    <div className="relative overflow-hidden">
-      <div className="flex space-x-4 overflow-x-auto scrollbar-hide snap-x snap-mandatory px-4">
-        {banners.map((src, i) => (
-          <img
-            key={i}
-            src={src}
-            alt={`banner-${i}`}
-            className="w-full flex-shrink-0 h-40 object-cover rounded-2xl snap-center"
-            onClick={() => setIdx(i)}
-          />
-        ))}
-      </div>
-      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-1">
-        {banners.map((_, i) => (
-          <span
-            key={i}
-            className={`
-              transition-all
-              ${idx === i
-                ? "w-3 h-3 bg-blue-500"
-                : "w-2 h-2 bg-white bg-opacity-70"
-              }
-              rounded-full border border-blue-200
-            `}
-            style={{ margin: "0 3px" }}
-          />
-        ))}
-      </div>
+    <div className="page-container">
+      <Swiper
+        className="banner h-48 rounded-lg"
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        modules={[Navigation, Pagination, Autoplay]}
+        autoplay={{ delay: 1200 }}
+      >
+        <SwiperSlide>
+          <img className="home-img" src={banner1} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="home-img" src={banner2} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="home-img" src={banner3} />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
