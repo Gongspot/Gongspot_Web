c528d0179d3d5c3b00fdc02af10e6440076a71a9
Seoyeon <seoyeoneel02@gmail.com>
2025-07-06 01:26:19 +0900

    refactor: 코드 리팩토링

--- src/components/myPage/MyPageItem.tsx
+++ src/components/myPage/MyPageItem.tsx
@@ -9,12 +9,13 @@ interface MyPageItemProps {
 
 const MyPageItem = ({ icon, title, value, onClick }: MyPageItemProps) => {
     return (
-        <div className="flex flex-col items-center text-[0.75rem] bg-white
-            rounded-[0.938rem] border border-[#E5E5E5] py-[0.75rem] w-full"
+        <div
+            className="w-full flex flex-col items-center bg-white 
+            border border-[#E5E5E5] rounded-[0.938rem] py-[0.75rem] text-[0.75rem]"
             onClick={onClick}
         >
-            <div className="mb-[0.438rem] flex items-center justify-center">
-                {icon}
+            <div className="flex items-center justify-center mb-[0.438rem]">
+            {icon}
             </div>
             <h3>{title}</h3>
             <span className="text-[#4CB1F1]">{value}</span>
--- src/components/myPage/ProfileSection.tsx
+++ src/components/myPage/ProfileSection.tsx
@@ -3,20 +3,22 @@ interface ProfileSectionProps {
     profile: string;
 }
 
-const ProfileSection = ({ nickname, profile }: ProfileSectionProps) => (
-    <div className="pt-[2.5rem]">
-        <div className="flex items-center m-[1.75rem] mb-[1.938rem]">
-            <span className="flex w-[2.938rem] h-[2.938rem] rounded-full bg-[#D9D9D9] items-center justify-center">
-                <img src={profile} alt="프로필" />
-            </span>
-            <span className="flex flex-col justify-center ml-[1.125rem]">
-                <h1 className="text-[1.438rem]">
-                    <span className="font-bold text-[#4CB1F1]">{nickname}</span>님
-                </h1>
-                <p className="text-[0.938rem] text-[#616161]">오늘 하루도 환영합니다!</p>
-            </span>
+const ProfileSection = ({ nickname, profile }: ProfileSectionProps) => {
+    return (
+        <div className="pt-[2.5rem]">
+            <div className="mb-[1.938rem] m-[1.75rem] flex items-center">
+                <span className="flex h-[2.938rem] w-[2.938rem] items-center justify-center rounded-full bg-[#D9D9D9]">
+                    <img src={profile} alt="프로필" />
+                </span>
+                <span className="ml-[1.125rem] flex flex-col justify-center">
+                    <h1 className="text-[1.438rem]">
+                        <span className="font-bold text-[#4CB1F1]">{nickname}</span>님
+                    </h1>
+                    <p className="text-[0.938rem] text-[#616161]">오늘 하루도 환영합니다!</p>
+                </span>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default ProfileSection;
