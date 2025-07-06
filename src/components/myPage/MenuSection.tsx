f345d1c7e2f8f34b760a4cb305787a52b23960c8
Seoyeon <seoyeoneel02@gmail.com>
2025-07-06 23:19:11 +0900

    refactor: 구분선 스타일 수정

--- src/components/myPage/MenuSection.tsx
+++ src/components/myPage/MenuSection.tsx
@@ -6,18 +6,18 @@ const MenuSection = () => {
         <>
             <Menu text="프로필 관리" />
             <SubMenu text="프로필 관리" />
-            <div className="h-[0.063rem] w-full mt-[1.25rem] border-b border-[#CCCCCC]" />
+            <div className="w-full mt-[1.25rem] border-b-[0.063rem] border-[#CCCCCC]" />
 
             <Menu text="공관 관리" />
             <div className="flex flex-col gap-[0.563rem]">
                 <SubMenu text="내가 방문한 공간" />
                 <SubMenu text="새 공간 등록 신청" />
             </div>
-            <div className="h-[0.063rem] w-full mt-[1.25rem] border-b border-[#CCCCCC]" />
+            <div className="w-full mt-[1.25rem] border-b-[0.063rem] border-[#CCCCCC]" />
 
             <Menu text="알림 설정" />
             <SubMenu text="알림 설정" />
-            <div className="h-[0.063rem] w-full mt-[1.25rem] border-b border-[#CCCCCC]" />
+            <div className="w-full mt-[1.25rem] border-b-[0.063rem] border-[#CCCCCC]" />
 
             <Menu text="포인트" />
             <div className="flex flex-col gap-[0.563rem]">
