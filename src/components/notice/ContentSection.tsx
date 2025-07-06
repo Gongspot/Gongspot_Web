45d01968c5c2742484dd567080ec86213a7e6eea
Seoyeon <seoyeoneel02@gmail.com>
2025-07-06 21:59:19 +0900

    refactor: 컴포넌트명 수정

--- src/components/notice/ContentSection.tsx
+++ src/components/notice/ContentSection.tsx
@@ -1,4 +1,4 @@
-const NoticeSection = () => {
+const ContentSection = () => {
     const content = "안녕하세요, 공스팟입니다. \n\n공스팟은 청년들을 위한 맞춤형 학습 공간을 추천해드리기 위해 만들어졌습니다. 학습 공간의 실시간 정보와 생생한 사용자 후기를 제공하여 모두가 각자의 공부 스타일과 목적에 맞는 학습 공간을 쉽게 찾을 수 있게 하고자 합니다.";
 
     return (
@@ -12,4 +12,4 @@ const NoticeSection = () => {
     );
 };
 
-export default NoticeSection;
+export default ContentSection;
