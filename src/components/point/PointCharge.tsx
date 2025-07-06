a2888b0d73465506a02410061542ac838e49addd
Seoyeon <seoyeoneel02@gmail.com>
2025-07-07 01:21:21 +0900

    feat: 포인트 충전 퍼블리싱

---
+++ src/components/point/PointCharge.tsx
@@ -0,0 +1,13 @@
+import ChargeSection from './ChargeSection';
+import TitleSection from './TitleSection';
+
+const PointCharge = () => {
+    return (
+        <>
+            <TitleSection title="포인트 충전하기" />
+            <ChargeSection />
+        </>
+    );
+};
+
+export default PointCharge;
