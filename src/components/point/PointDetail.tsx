d790e4ca7ba4d4fffba77bcdb60e01033d199b71
Seoyeon <seoyeoneel02@gmail.com>
2025-07-07 02:13:08 +0900

    feat: 포인트 내역 퍼블리싱

---
+++ src/components/point/PointDetail.tsx
@@ -0,0 +1,13 @@
+import DetailSection from './DetailSection';
+import TitleSection from './TitleSection';
+
+const PointDetail = () => {
+    return (
+        <>
+            <TitleSection title="포인트 내역" />
+            <DetailSection />
+        </>
+    );
+};
+
+export default PointDetail;
