d1ba2b0fd1de9dda7b3c018b8fc50a1c84fc2c25
Seoyeon <seoyeoneel02@gmail.com>
2025-07-07 22:03:02 +0900

    refactor: 상단바 디자인 수정

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import back from '../assets/topBack.svg';
 
 interface TopHeaderProps {
   title: string;          
@@ -9,10 +10,11 @@ const TopHeader = ({ title, backButton = true }: TopHeaderProps) => {
   const navigate = useNavigate();
 
   return (
-    <div className="fixed top-0 left-0 w-full bg-white flex items-center px-4 h-10 shadow-md z-30">
+    <div className="fixed top-0 left-0 w-full bg-white flex items-center px-[1rem] h-10 shadow-md z-30"
+      style={{ boxShadow: '0px 4px 10px 0px #0000000D' }}>
       {backButton && (
-        <button onClick={() => navigate(-1)} className="z-10 font-bold text-lg px-2">
-          ‹
+        <button onClick={() => navigate(-1)} className="z-10 font-bold text-lg">
+          <img src={back} alt="이전" />
         </button>
       )}
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-sm font-semibold">
