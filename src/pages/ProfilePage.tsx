effd6c86ab9faeeeb5220b13893e8d22ed07f9c5
Seoyeon <seoyeoneel02@gmail.com>
2025-07-09 02:30:37 +0900

    refactor: 컴포넌트 분리

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -3,6 +3,7 @@ import Profile from "../components/profile/Profile";
 import NextButton from "../components/NextButton";
 import TopHeader from "../components/TopHeader";
 import { useState } from "react";
+import ActionSheet from "../components/profile/ActionSheet";
 
 const nickname = "카공족";
 
@@ -25,23 +26,23 @@ const ProfilePage = () => {
           <div
             className="fixed inset-0 bg-black opacity-15 z-40"
           />
-          <div className="fixed bottom-0 w-full px-[1rem] pb-[1.5rem] z-50">
-            <button
-              className="w-full bg-[#EFEFEF] text-[#007AFF] text-[1.125rem] rounded-[0.75rem]"
-            >
-              <p className="py-[1.125rem]">지금 촬영하기</p>
-              <div className="w-full border-b-[0.063rem] border-[#D9D9D9]" />
-              <p className="py-[1.125rem]">앨범에서 선택하기</p>
-              <div className="w-full border-b-[0.063rem] border-[#D9D9D9]" />
-              <p className="py-[1.125rem] text-[#FF3B30]">현재 사진 지우기</p>
-            </button>
-            <button
-              className="w-full mt-[1rem] py-[1rem] bg-white text-[#007AFF] text-[1.125rem] font-medium rounded-[0.75rem]"
-              onClick={() => setOverlayActive(false)}
-            >
-              취소
-            </button>
-          </div>
+          <ActionSheet
+            onTake={() => {
+              alert("사진을 촬영합니다.");
+              setOverlayActive(false);
+            }}
+            onSelect={() => {
+              alert("앨범에서 사진을 선택합니다.");
+              setOverlayActive(false);
+            }}
+            onRemove={() => {
+              alert("현재 사진을 지웁니다.");
+              setOverlayActive(false);
+            }}
+            onCancel={() => {
+              setOverlayActive(false);
+            }}
+          />
         </>
       )}
     </div>
