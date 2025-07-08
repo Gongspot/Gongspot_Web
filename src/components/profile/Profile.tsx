ed1e7df7836b61bfd0f1c5c8cedc86125f096296
Seoyeon <seoyeoneel02@gmail.com>
2025-07-09 02:01:52 +0900

    feat: 프로필 버튼 구현

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,15 +1,21 @@
 import profile from "../../assets/profile.svg";
 import camera from "../../assets/camera.svg";
 
-const Profile = () => {
+interface ProfileProps {
+  onClick: () => void;
+}
+
+const Profile = ({ onClick }: ProfileProps) => {
     return (
         <div className="mt-[2.5rem]">
             <div className="pt-[2.75rem] flex items-center justify-center">
                 <span className="flex relative w-[5.25rem] h-[5.25rem] items-center justify-center 
                 rounded-full bg-[#D9D9D9]">
                     <img src={profile} alt="프로필" className="w-[3rem] h-[3rem]" />
-                    <span className="flex w-[1.625rem] h-[1.625rem] items-center justify-center rounded-full 
-                    bg-white border-[0.063rem] border-[#D9D9D9] absolute bottom-0 right-0">
+                    <span 
+                        className="flex w-[1.625rem] h-[1.625rem] items-center justify-center rounded-full 
+                        bg-white border-[0.063rem] border-[#D9D9D9] absolute bottom-0 right-0"
+                        onClick={onClick}>
                         <img src={camera} alt="카메라" />
                     </span>
                 </span>
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,15 +1,49 @@
 import Nickname from "../components/profile/Nickname";
 import Profile from "../components/profile/Profile";
+import NextButton from "../components/NextButton";
 import TopHeader from "../components/TopHeader";
+import { useState } from "react";
 
 const nickname = "카공족";
 
 const ProfilePage = () => {
+  const [overlayActive, setOverlayActive] = useState(false);
+
   return (
     <div className="flex flex-col h-screen w-full bg-white">
       <TopHeader title="프로필 관리" backButton={true} />
-      <Profile />
+      <Profile onClick={() => setOverlayActive(true)} />
       <Nickname nickname={nickname} />
+      <NextButton
+        text="변경하기"
+        onClick={() => {
+          alert("프로필이 변경되었습니다.");
+        }}
+      />
+      {overlayActive && (
+        <>
+          <div
+            className="fixed inset-0 bg-black opacity-15 z-40"
+          />
+          <div className="fixed bottom-0 w-full px-[1rem] pb-[1.5rem] z-50">
+            <button
+              className="w-full bg-[#EFEFEF] text-[#007AFF] text-[1.125rem] rounded-[0.75rem]"
+            >
+              <p className="py-[1.125rem]">지금 촬영하기</p>
+              <div className="w-full border-b-[0.063rem] border-[#D9D9D9]" />
+              <p className="py-[1.125rem]">앨범에서 선택하기</p>
+              <div className="w-full border-b-[0.063rem] border-[#D9D9D9]" />
+              <p className="py-[1.125rem] text-[#FF3B30]">현재 사진 지우기</p>
+            </button>
+            <button
+              className="w-full mt-[1rem] py-[1rem] bg-white text-[#007AFF] text-[1.125rem] font-medium rounded-[0.75rem]"
+              onClick={() => setOverlayActive(false)}
+            >
+              취소
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
