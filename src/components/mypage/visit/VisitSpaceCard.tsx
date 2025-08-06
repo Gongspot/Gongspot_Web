fd622fc0f41b0a9266a8da4369c4b86d9e017044
Seoyeon <seoyeoneel02@gmail.com>
2025-08-06 16:52:27 +0900

    feat: 상세보기 링크 연결

--- src/components/mypage/visit/VisitSpaceCard.tsx
+++ src/components/mypage/visit/VisitSpaceCard.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom';
 import Likes from '../../../assets/likes.svg?react';
 
 interface VisitSpaceCardProps {
+  placeId: number;
   name: string;
   image: string;
   rate: number | null;
@@ -10,12 +12,15 @@ interface VisitSpaceCardProps {
 }
 
 const VisitSpaceCard = ({
+  placeId,
   name,
   image,
   rate,
   visitedDate,
   type,
+  isLiked
 }: VisitSpaceCardProps) => {
+  const navigate = useNavigate();
   const formatDate = (dateStr: string) => {
     const [year, month, day] = dateStr.split('-');
     return `${year.slice(2)}.${month}.${day}`;
@@ -24,7 +29,7 @@ const VisitSpaceCard = ({
   const formattedDate = formatDate(visitedDate);
 
   return (
-    <div className="flex flex-col pl-[1.25rem] py-[0.875rem] bg-white">
+    <div className="flex flex-col pl-[1.25rem] mb-[0.375rem] py-[0.875rem] bg-white">
         <p className="text-[1rem] font-medium">{formattedDate}</p>
         <div className="flex items-center justify-start mt-[0.625rem]">
           <div className="flex relative">
@@ -32,7 +37,7 @@ const VisitSpaceCard = ({
               src={image}
               alt={name}
               className="w-[10rem] h-[7.5rem] bg-[#B1B8C180] rounded-[0.938rem] border-[0.063rem] border-[#B1B8C180]" />
-            <Likes className="absolute top-[0.625rem] right-[0.625rem] text-[#4CB1F1]" />
+            <Likes className={`absolute top-[0.625rem] right-[0.625rem] ${isLiked ? 'text-[#4CB1F1]' : 'text-white'}`} />
           </div>
           <div className="flex flex-col pl-[1.125rem] text-[0.813rem]">
             <p className="text-[0.938rem] font-semibold">{name}</p>
@@ -40,7 +45,8 @@ const VisitSpaceCard = ({
             <p>0.2km</p>
             <p>#{type}</p>
             <button
-                className="mt-[0.5rem] px-[3.125rem] py-[0.5rem] bg-[#4CB1F1] text-white text-[0.813rem] font-medium rounded-[1.25rem]"
+              className="mt-[0.5rem] px-[3.125rem] py-[0.5rem] bg-[#4CB1F1] text-white text-[0.813rem] font-medium rounded-[1.25rem]"
+              onClick={() => navigate(`/space/${placeId}`)}
             >
               상세보기
             </button>
--- src/pages/mypage/VisitPage.tsx
+++ src/pages/mypage/VisitPage.tsx
@@ -26,18 +26,16 @@ const VisitPage = () => {
       <TopHeader title="방문 공간" backButton={true} />
       <div>
         {visitedPlaces.map((item, idx) => (
-          <>
-            <VisitSpaceCard
-              key={idx}
-              name={item.name}
-              image="https://images.unsplash.com/photo-1650338031221-ba71d1178d43?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              rate={item.rate}
-              visitedDate={item.visitedDate}
-              type={item.type}
-              isLiked={item.isLiked}
-            />
-            <div className="mt-[0.375rem]" />
-          </>
+          <VisitSpaceCard
+            key={idx}
+            placeId={item.placeId}
+            name={item.name}
+            image="https://images.unsplash.com/photo-1650338031221-ba71d1178d43?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            rate={item.rate}
+            visitedDate={item.visitedDate}
+            type={item.type}
+            isLiked={item.isLiked}
+          />
         ))}
       </div>
     </div>
