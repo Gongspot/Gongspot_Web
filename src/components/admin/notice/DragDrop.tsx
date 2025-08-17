c75765eefc9e8a2bfec9e964700d668086d515b9
Seoyeon <seoyeoneel02@gmail.com>
2025-08-18 01:39:53 +0900

    refactor: 파일첨부 UI 수정

--- src/components/admin/notice/DragDrop.tsx
+++ src/components/admin/notice/DragDrop.tsx
@@ -1,4 +1,5 @@
 import Cloud from '../../../assets/cloud.svg?react';
+import Delete from '../../../assets/delete.svg?react';
 
 interface DragDropProps {
     files: File[] | null;
@@ -11,31 +12,56 @@ const DragDrop = ({ files, onFileChange }: DragDropProps) => {
         onFileChange(selectedFiles);
     };
 
+    const handleDelete = (idx: number) => {
+        if (!files) return;
+        const updatedFiles = files.filter((_, i) => i !== idx);
+        onFileChange(updatedFiles.length > 0 ? updatedFiles : null);
+    };
+
     return (
-        <div className="flex flex-col space-y-[0.5rem] items-center justify-center
-            border border-dashed border-[#E5E5E5] rounded-[0.5rem] py-[1.125rem]">
-            <Cloud />
-            <p className="text-[0.75rem] text-[#ADAEBC]">파일을 드래그하거나 클릭하여 업로드</p>
-            <label
-                htmlFor="noticeFile"
-                className="bg-[#F5F5F5] px-[0.625rem] py-[0.25rem] text-[0.75rem] rounded-[0.313rem]"
-            >
-                파일 선택
-                <input
-                    id="noticeFile"
-                    type="file"
-                    name="noticeFile"
-                    className="hidden"
-                    multiple
-                    onChange={handleFileChange}
-                />
-            </label>
+        <>
+            <div className="flex flex-col space-y-[0.5rem] items-center justify-center
+                border border-dashed border-[#E5E5E5] rounded-[0.5rem] py-[1.125rem]">
+                <Cloud />
+                <p className="text-[0.75rem] text-[#ADAEBC]">파일을 드래그하거나 클릭하여 업로드</p>
+                <label
+                    htmlFor="noticeFile"
+                    className="bg-[#F5F5F5] px-[0.625rem] py-[0.25rem] text-[0.75rem] rounded-[0.313rem]"
+                >
+                    파일 선택
+                    <input
+                        id="noticeFile"
+                        type="file"
+                        name="noticeFile"
+                        className="hidden"
+                        multiple
+                        onChange={handleFileChange}
+                    />
+                </label>
+            </div>
             {files && (
-                <div className="text-[0.75rem] text-black">
-                    <p>선택된 파일: {files.map(file => file.name).join(", ")}</p>
+                <div className="mt-[1rem]">
+                    <ul className="flex flex-wrap gap-x-[0.5rem] gap-y-[1rem]">
+                        {files.map((file, idx) => (
+                            <li 
+                                key={idx}
+                                className="flex items-center py-[0.25rem] border border-[#E5E5E5] rounded-[1.25rem] 
+                                    text-[#8F9098] text-[0.75rem] text-center break-words"
+                            >
+                                <span className="ml-[1rem]">{file.name}</span>
+                                <button
+                                    type="button"
+                                    onClick={() => handleDelete(idx)}
+                                    className="flex items-center mx-[0.625rem]"
+                                >
+                                    <Delete />
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             )}
-        </div>
+        </>
     );
 };
 
