a7745020767d10cef0cde673725cd80bbaa100a8
Seoyeon <seoyeoneel02@gmail.com>
2025-08-21 15:53:21 +0900

    refactor: 기본 프로필 사이즈 조정

--- src/components/mypage/ProfileSection.tsx
+++ src/components/mypage/ProfileSection.tsx
@@ -5,6 +5,7 @@ import defaultProfile from "../../assets/profile.svg";
 const ProfileSection = () => {
     const [nickname, setNickname] = useState<string | null>(null);
     const [profile, setProfile] = useState<string>(defaultProfile);
+    const isDefault = profile === defaultProfile;
 
     useEffect(() => {
         const fetchProfile = async () => {
@@ -24,8 +25,10 @@ const ProfileSection = () => {
     return (
         <div className="pt-0">
             <div className="m-[1.75rem] flex items-center">
-                <span className="flex h-[2.938rem] w-[2.938rem] items-center justify-center rounded-full bg-[#D9D9D9]">
-                    <img src={profile} alt="프로필" className="w-full h-full object-cover rounded-full" />
+                <span className="flex h-[3rem] w-[3rem] items-center justify-center rounded-full bg-[#D9D9D9]">
+                    <img 
+                        src={profile} alt="프로필"
+                        className={`object-cover rounded-full ${isDefault ? 'w-[1.5rem] h-[1.5rem]' : 'w-full h-full'}`} />
                 </span>
                 <span className="ml-[1.125rem] flex flex-col justify-center font-medium">
                     <h1 className="text-[1.438rem]">
