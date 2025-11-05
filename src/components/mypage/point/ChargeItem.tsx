5b71a347c4559d83a1cca180c510633c92649131
Seoyeon <seoyeoneel02@gmail.com>
2025-11-06 02:51:06 +0900

    refactor: 포인트 기능 리팩토링

--- src/components/mypage/point/ChargeItem.tsx
+++ src/components/mypage/point/ChargeItem.tsx
@@ -8,6 +8,7 @@ const ChargeItem = ({ point, price }: ChargeItemProps) => {
         <>
             <div
             className="my-[1.125rem] flex items-center justify-between px-[1.875rem] text-[0.938rem]"
+            onClick={() => alert("포인트 충전 기능은 추후 구현 예정입니다.")}
             >
                 {point}P
                 <button className="rounded-[1.25rem] bg-[#4CB1F1] px-[0.563rem] py-[0.25rem] text-white">
--- src/components/mypage/point/MainList.tsx
+++ src/components/mypage/point/MainList.tsx
@@ -4,25 +4,23 @@ import next from '../../../assets/next.svg';
 const MainList = () => {
     return (
         <>
-            <div
-            className="flex items-center justify-between pt-[1.125rem] px-[1.75rem] my-[1.125rem] 
-            text-[0.938rem]"
+            <Link
+                to="/mypage/point/charge"
+                className="flex items-center justify-between pt-[1.125rem] px-[1.75rem] my-[1.125rem] 
+                text-[0.938rem]"
             >
                 포인트 충전하기
-                <Link to="/mypage/point/charge" className="flex items-center">
-                    <img src={next} alt="다음" />
-                </Link>
-            </div>
+                <img src={next} alt="다음" />
+            </Link>
             <div className="h-[0.063rem] w-full border-b border-[#B1B8C154]" />
-            <div
-            className="flex items-center justify-between px-[1.75rem] my-[1.125rem] 
-            text-[0.938rem]"
+            <Link
+                to="/mypage/point/detail"
+                className="flex items-center justify-between px-[1.75rem] my-[1.125rem] 
+                text-[0.938rem]"
             >
                 포인트 내역
-                <Link to="/mypage/point/detail" className="flex items-center">
                 <img src={next} alt="다음" />
-                </Link>
-            </div>
+            </Link>
             <div className="h-[0.063rem] w-full border-b border-[#B1B8C154]" />
         </>
     );
